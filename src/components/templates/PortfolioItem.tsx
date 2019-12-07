50affd50362b0e65ea1687e7b31dcc7ca3143f98
Jacob Herper <jacobherper@Jacobs-MacBook-Pro.local>
2019-12-07 14:35:16 +0000

    Portfolio added

---
+++ src/components/templates/PortfolioItem.tsx
@@ -0,0 +1,86 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
+import { BLOCKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+import Layout from "./Layout"
+import SEO from "../molecules/Seo"
+
+const PortfolioItem = ({ data, pageContext }: any) => {
+  const post = data.contentfulItem
+  // const siteTitle = this.props.data.site.siteMetadata.title;
+  const { previous, next } = pageContext
+
+  return (
+    <Layout>
+      <SEO title={post.title} description={post.description} />
+      <Img
+        fluid={post.featured_image.fluid}
+        alt={post.title}
+        style={{ maxHeight: "400px" }}
+      />
+      <div className="container" style={{ marginTop: "2rem" }}>
+        <h2>{post.title}</h2>
+        <h3>{post.type}</h3>
+        <div>{documentToReactComponents(post.content.json)}</div>
+        <p>
+          <strong>Technologies used:</strong> {post.technologies.join(", ")}
+        </p>
+        <a
+          href={post.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button"
+        >
+          Visit project
+        </a>
+        <ul className="pagination">
+          {previous && (
+            <li>
+              <Link to={`/${previous.slug}/`} rel="prev">
+                &laquo; {previous.title}
+              </Link>
+            </li>
+          )}
+          {next && (
+            <li>
+              <Link to={`/${next.slug}/`} rel="next">
+                {next.title} &raquo;
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </Layout>
+  )
+}
+
+export default PortfolioItem
+
+export const pageQuery = graphql`
+  query PortfolioItemBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    contentfulItem(slug: { eq: $slug }) {
+      title
+      client
+      type
+      technologies
+      url
+      description
+      content {
+        json
+      }
+      featured_image {
+        fluid(maxWidth: 1920, quality: 70) {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+`
---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,74 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/templates/Layout"
+import SEO from "../components/molecules/Seo"
+
+const Portfolio = ({ data }: any) => {
+  const posts = data.allContentfulItem.edges
+
+  return (
+    <Layout>
+      <SEO title="Portfolio" />
+      <div className="container" style={{ marginTop: "2rem" }}>
+        <h2>
+          Portfolio<span className="accent">.</span>
+        </h2>
+        <div className="wrapper">
+          {posts.map(({ node }: any) => {
+            const title = node.title || node.slug
+            return (
+              <div key={node.slug} className="portfolio-item">
+                <Link to={`/${node.slug}/`}>
+                  <Img fluid={node.featured_image.fluid} />
+                  <div className="overlay">
+                    <h3>{title}</h3>
+                    <small>{node.type}</small>
+                  </div>
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+        <p style={{ textAlign: `center`, marginTop: `2rem` }}>
+          This is a small selection of the hundreds of projects I worked on.
+          <br />
+          Want to see more?{" "}
+          <Link to="/contact/">
+            <strong>Get in touch</strong>
+          </Link>{" "}
+          and let me know what you're looking for, so I can send you a more
+          comprehensive portfolio.
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default Portfolio
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulItem(sort: { fields: sortOrder, order: ASC }) {
+      edges {
+        node {
+          title
+          slug
+          client
+          type
+          featured_image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
