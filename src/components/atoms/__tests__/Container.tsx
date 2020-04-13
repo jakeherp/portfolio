760abb42cb45ed31ad05925e7ef872b9a070df02
Jacob Herper <jacob@herper.uk>
2020-04-13 10:51:17 +0100

    Switched snapshots

--- src/components/atoms/__tests__/Container.tsx
+++ src/components/atoms/__tests__/Container.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-
+import 'jest-styled-components';
+import { render } from '@testing-library/react';
 import Container from '../Container';
 
 it('renders correctly', () => {
-  const tree = renderer
-    .create(
-      <Container>
-        <div />
-      </Container>
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  const { container } = render(
+    <Container>
+      <div />
+    </Container>
+  );
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    .c0 {
+      width: 1140px;
+      max-width: 94%;
+      margin: 0 auto;
+    }
+
+    <div
+      class="c0"
+    >
+      <div />
+    </div>
+  `);
 });
--- src/components/atoms/__tests__/Highlight.tsx
+++ src/components/atoms/__tests__/Highlight.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-
+import 'jest-styled-components';
+import { render } from '@testing-library/react';
 import Highlight from '../Highlight';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Highlight>Highlighted text</Highlight>)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+test('renders correctly', () => {
+  const { container } = render(<Highlight>Highlighted text</Highlight>);
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    .c0 {
+      color: #e94e1b;
+    }
+
+    <span
+      class="c0"
+    >
+      Highlighted text
+    </span>
+  `);
 });
