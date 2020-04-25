fa6e2fe5f05c9d961d81591bae7866e4e530e24f
Jacob Herper <jacob@herper.uk>
2020-04-25 18:55:23 +0100

    Portfolio test TS error fixed

--- src/pages/__tests__/portfolio.test.tsx
+++ src/pages/__tests__/portfolio.test.tsx
@@ -3,7 +3,45 @@ import * as Gatsby from 'gatsby';
 import 'jest-styled-components';
 import { render } from '@testing-library/react';
 import Portfolio from '../portfolio';
-import mockData from '../../../__mocks__/portfolio-mock';
+
+const mockData = {
+  allContentfulItem: {
+    edges: [
+      {
+        node: {
+          title: 'Portfolio item 1',
+          slug: 'portfolio-item-1',
+          image: {
+            fluid: {
+              base64: '',
+              aspectRatio: 1.5,
+              src: '/',
+              srcSet: '/',
+              sizes: '(max-width: 800px) 100vw, 800px',
+            },
+          },
+          type: 'item',
+        },
+      },
+      {
+        node: {
+          title: 'Portfolio item 2',
+          slug: 'portfolio-item-2',
+          image: {
+            fluid: {
+              base64: '',
+              aspectRatio: 1.5,
+              src: '/',
+              srcSet: '/',
+              sizes: '(max-width: 800px) 100vw, 800px',
+            },
+          },
+          type: 'item',
+        },
+      },
+    ],
+  },
+};
 
 const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery');
 useStaticQuery.mockImplementation(() => ({
