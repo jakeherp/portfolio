ab4279f3c385e678afcaded49a08f8f2469c1cfe
Jacob Herper <jacob@herper.uk>
2020-04-26 18:39:53 +0100

    Additional app tests added

--- cypress/e2e/app.test.js
+++ cypress/e2e/app.test.js
@@ -1,10 +1,26 @@
 /// <reference types="Cypress" />
 
 describe('Portfolio app', () => {
-  it('renders the application', () => {
+  beforeEach(() => {
     cy.visit('/');
-    // .get(/.Layout__Wrapper-*/gi)
-    // .children()
-    // .should('have.length', '3');
+  });
+
+  it('loads home page', () => {
+    cy.get('main h1');
+  });
+
+  it('navigates to about page and back', () => {
+    cy.getTestElement('MenuToggle')
+      .click()
+      .get('header ul > li:nth-of-type(2) > a')
+      .click()
+      .url()
+      .should('match', /about/);
+
+    // go back to home page
+    cy.get('header h1')
+      .click()
+      .url()
+      .should('include', Cypress.config('baseUrl'));
   });
 });
