7ebf09796031a7ff6c318638b2aecb365a53a6d9
Jacob Herper <jacob@herper.uk>
2022-01-08 22:46:13 +0000

    hardcoded sentry dsn for now

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,6 +7,8 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN;
 
 Sentry.init({
-	dsn: SENTRY_DSN || '',
+	dsn:
+		SENTRY_DSN ||
+		'https://60777b40fa4e4b009ee06b34589f2139@o1111192.ingest.sentry.io/6140295',
 	tracesSampleRate: 1.0,
 });
