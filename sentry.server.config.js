aa7b3d634c1f1f81589ea4394540ff00be81723e
Jacob Herper <jacob@herper.uk>
2022-01-08 23:00:45 +0000

    reverts change

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,8 +7,6 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN;
 
 Sentry.init({
-	dsn:
-		SENTRY_DSN ||
-		'https://60777b40fa4e4b009ee06b34589f2139@o1111192.ingest.sentry.io/6140295',
+	dsn: SENTRY_DSN || '',
 	tracesSampleRate: 1.0,
 });
--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -35,19 +35,6 @@ MyError.getInitialProps = async (context: NextPageContext) => {
 		return errorInitialProps;
 	}
 
-	// Running on the server, the response object (`res`) is available.
-	//
-	// Next.js will pass an err on the server if a page's data fetching methods
-	// threw or returned a Promise that rejected
-	//
-	// Running on the client (browser), Next.js will provide an err if:
-	//
-	//  - a page's `getInitialProps` threw or returned a Promise that rejected
-	//  - an exception was thrown somewhere in the React lifecycle (render,
-	//    componentDidMount, etc) that was caught by Next.js's React Error
-	//    Boundary. Read more about what types of exceptions are caught by Error
-	//    Boundaries: https://reactjs.org/docs/error-boundaries.html
-
 	if (err) {
 		Sentry.captureException(err);
 
