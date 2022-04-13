38487065cf347fac8881cc2d49ec0795dce53cb2
Jacob Herper <jacob@herper.uk>
2022-04-13 22:26:46 +0100

    test: fixes tests

--- src/components/molecules/MobileMenu/MobileMenu.test.tsx
+++ src/components/molecules/MobileMenu/MobileMenu.test.tsx
@@ -1,6 +1,16 @@
 import { MobileMenu } from '../MobileMenu';
 import { render } from '@testing-library/react';
 
+jest.mock('next/router', () => ({
+	__esModule: true,
+	useRouter: jest.fn().mockReturnValue({
+		route: '',
+		pathname: '',
+		query: '',
+		asPath: '',
+	}),
+}));
+
 describe('MobileMenu', () => {
 	it('renders correctly', () => {
 		const { container } = render(<MobileMenu isOpen={true} />);
