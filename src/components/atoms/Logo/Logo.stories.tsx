97771ea11471093ca890eef8e2b7650de60082b5
Jacob Herper <jacob@herper.uk>
2022-09-03 10:50:20 +0100

    chore: fixes logo story

--- src/components/atoms/Logo/Logo.stories.tsx
+++ src/components/atoms/Logo/Logo.stories.tsx
@@ -1,11 +1,10 @@
-import { Logo, LogoProps } from './Logo';
+import { Logo } from './Logo';
 
 export default {
 	title: 'atoms/Logo',
 	component: Logo,
 };
 
-export const LogoStory = (args: LogoProps) => <Logo {...args} />;
+export const LogoStory = () => <Logo />;
 
 LogoStory.storyName = 'Logo';
-LogoStory.args = {};
