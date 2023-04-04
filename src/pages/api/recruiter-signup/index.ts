c2bcc979a19b3e00e003a23197d03b618623c47a
Jacob Herper <jacob@herper.uk>
2023-04-04 12:42:17 +0100

    fix(recruiter-signup): fixes path to api route

--- src/pages/api/recruiter-signup/index.ts
+++ src/pages/api/recruiter-signup/index.ts
@@ -17,6 +17,12 @@ mailchimp.setConfig({
 const signupHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { firstName, lastName, company, email, type } = req.body;
 
+	if (req.method !== 'POST') {
+		return res
+			.status(405)
+			.json({ error: `Method '${req.method}' not allowed` });
+	}
+
 	if (!email) {
 		return res.status(400).json({ error: 'Email is required' });
 	}
