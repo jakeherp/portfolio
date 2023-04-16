1ab54c38f6744b61b1302e42ccecb9af1db1bd5d
Jacob Herper <jacob@herper.uk>
2023-04-16 14:36:16 +0100

    feat(sitemap): adds dynamic sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,38 @@
+import { sanityClient } from '@lib/sanity';
+
+import { postsQuery } from '@queries/posts';
+import { caseStudiesQuery } from '@root/src/queries/caseStudies';
+import { CaseStudy, Post } from '@root/src/types';
+
+export default async function sitemap() {
+	const blogPosts: Post[] = await sanityClient.fetch(postsQuery());
+	const caseStudies: CaseStudy[] = await sanityClient.fetch(caseStudiesQuery());
+
+	const blogRoutes = blogPosts.map(({ slug, publishedAt }) => ({
+		url: `https://www.jacobherper.com/blog/${slug}`,
+		lastModified: new Date(publishedAt).toISOString().split('T')[0],
+		priority: 0.65,
+	}));
+	const caseStudiesRoutes = caseStudies.map(({ slug, publishedAt }) => ({
+		url: `https://www.jacobherper.com/case-studies/${slug}`,
+		lastModified: new Date(publishedAt).toISOString().split('T')[0],
+		priority: 0.65,
+	}));
+	const staticRoutes = [
+		'',
+		'/about',
+		'/blog',
+		'/case-studies',
+		'/contact',
+		'/recruiters',
+		'/uses',
+	].map((route) => {
+		return {
+			url: `https://www.jacobherper.com${route}`,
+			lastModified: new Date().toISOString().split('T')[0],
+			priority: route === '' ? 1 : 0.8,
+		};
+	});
+
+	return [...staticRoutes, ...blogRoutes, ...caseStudiesRoutes];
+}
