2d15212d87c1be1091d5e489f87f7141275c2fcf
Jacob Herper <jacob@herper.uk>
2023-04-21 23:30:53 +0100

    feat(services): adds service component

---
+++ src/components/atoms/Service/Service.test.tsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import { Service } from './Service';
+
+jest.mock('@lib/sanity', () => ({
+	sanityClient: {
+		fetch: jest.fn(),
+	},
+}));
+
+describe('Service', () => {
+	it('should render', () => {
+		const defaultProps = {
+			title: 'Service',
+			subtitle: 'Some Subtitle',
+			slug: 'service',
+			body: [],
+			image: 'https://via.placeholder.com/900x600',
+		};
+
+		const { container } = render(<Service {...defaultProps} />);
+
+		expect(container.firstChild).toMatchSnapshot();
+	});
+});
---
+++ src/components/atoms/Service/Service.tsx
@@ -0,0 +1,50 @@
+import { BlockContent } from '@types';
+import Image from 'next/image';
+import { ContentBlock } from '../ContentBlock';
+import classNames from 'classnames';
+
+interface ServiceProps {
+	title: string;
+	subtitle: string;
+	slug: string;
+	body: BlockContent[];
+	image: string;
+	isReversed?: boolean;
+}
+
+export const Service = ({
+	title,
+	subtitle,
+	slug,
+	body,
+	image,
+	isReversed,
+}: ServiceProps) => {
+	return (
+		<article id={slug}>
+			<h3 className="headline mt-20 text-xl md:text-2xl lg:text-4xl">
+				{title}
+			</h3>
+			<div
+				className={classNames('flex flex-col lg:gap-12', {
+					'lg:flex-row-reverse': isReversed,
+					'lg:flex-row': !isReversed,
+				})}
+			>
+				<Image
+					src={image}
+					width={900}
+					height={600}
+					alt={`Illustration for ${title}`}
+					className="mt-8 px-12 lg:mt-0 lg:w-1/4 lg:px-0"
+				/>
+				<div>
+					<p className="mt-4 text-xl font-semibold md:text-xl lg:text-2xl">
+						{subtitle}
+					</p>
+					<ContentBlock value={body} />
+				</div>
+			</div>
+		</article>
+	);
+};
